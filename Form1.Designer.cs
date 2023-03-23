d17b68e03a3221d00ba474b4cdbd63b523c795e8
yunas-x <113031087+yunas-x@users.noreply.github.com>
2023-03-24 03:55:39 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace SimpleHTTPClient
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPick = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonPick
+            // 
+            this.buttonPick.Location = new System.Drawing.Point(12, 62);
+            this.buttonPick.Name = "buttonPick";
+            this.buttonPick.Size = new System.Drawing.Size(161, 79);
+            this.buttonPick.TabIndex = 0;
+            this.buttonPick.Text = "Choose File...";
+            this.buttonPick.UseVisualStyleBackColor = true;
+            this.buttonPick.Click += new System.EventHandler(this.buttonPick_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Location = new System.Drawing.Point(209, 62);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(161, 79);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 24);
+            this.label1.MaximumSize = new System.Drawing.Size(200, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 2;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 153);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonPick);
+            this.MaximumSize = new System.Drawing.Size(400, 200);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "ClientForm";
+            this.Text = "Simple Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPick;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using RestSharp;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleHTTPClient
+{
+    public partial class ClientForm : Form
+    {
+        public ClientForm()
+        {
+            InitializeComponent();
+        }
+
+        private string chosenFileName;
+        private string URL = "http://127.0.0.1:8000/classify";
+
+        async private void buttonSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrWhiteSpace(chosenFileName))
+                {
+                    if (File.Exists(chosenFileName))
+                    {
+                        await Task.Run(() => UploadFileWithRestSharpAsync());
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("File is deleted or moved");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Choose file");
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("File is opened and couldn't be proceeded");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                ToDefault();
+            }
+
+        }
+
+        private void ToDefault()
+        {
+            label1.Text = String.Empty;
+            chosenFileName = String.Empty;
+            buttonSend.Enabled = false;
+        }
+
+        private async Task UploadFileWithRestSharpAsync()
+        {
+            var client = new RestClient("http://localhost:8000");
+
+            var request = new RestRequest("/classify")
+                .AddFile("file", chosenFileName, "multipart/form-data");
+
+
+            var response = await client.PostAsync(request);
+
+            var json = JsonDocument.Parse(response.Content);
+
+            MessageBox.Show(json.RootElement.GetProperty("txt").GetString().Substring(0, 50));
+            MessageBox.Show(json.RootElement.GetProperty("file-name").GetString().Substring(0, 50));
+        }
+
+        private void buttonPick_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Microsoft Word (*.docx)|*.docx";
+            openFileDialog.Title = "Choose Files";
+            openFileDialog.ShowDialog();
+
+            if (!String.IsNullOrEmpty(openFileDialog.FileName))
+            {
+                label1.Text = openFileDialog.FileName;
+                chosenFileName = openFileDialog.FileName;
+                buttonSend.Enabled = true;
+            }
+
+        }
+    }
+}
